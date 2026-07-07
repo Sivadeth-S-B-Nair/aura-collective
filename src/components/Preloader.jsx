33cc6ef677455c664a497e181ebb6a6f3cee0be9
Sivadeth <sivadeth@evalogical.com>
2026-07-07 15:03:50 +0530

    basic preloader

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -9,42 +9,52 @@ export default function Preloader({ onComplete }) {
   const headlineRef = useRef(null);
 
   useGSAP(
-    () => {
+    (context, contextSafe) => {
       const split = new SplitText(headlineRef.current, {
         type: "chars",
         charsClass: "char",
       });
 
-      // 1. Scatter every character to a random position + 3D rotation.
       gsap.set(split.chars, {
-        x: () => gsap.utils.random(-800, 800),
-        y: () => gsap.utils.random(-800, 800),
-        z: () => gsap.utils.random(-800,800),
-        rotationX: () => gsap.utils.random(-180, 180),
-        rotationY: () => gsap.utils.random(-180, 180),
-        rotationZ: () => gsap.utils.random(-90, 90),
+        x: () => gsap.utils.random(-1200, 1200),
+        y: () => gsap.utils.random(-900, 900),
+        z: () => gsap.utils.random(-1000, 400),
+        rotationX: () => gsap.utils.random(-220, 220),
+        rotationY: () => gsap.utils.random(-220, 220),
+        rotationZ: () => gsap.utils.random(-120, 120),
         opacity: 0,
+        filter: "blur(18px)",
       });
 
-      // 2. Only assemble once real assets are ready. Right now that's just
-      //    the webfont; later phases will add image/model preloading here.
-      Promise.all([document.fonts.ready]).then(() => {
-        gsap
-          .timeline({ onComplete: () => onComplete?.() })
-          .to(split.chars, {
-            x: 0,
-            y: 0,
-            z: 0,
-            rotationX: 0,
-            rotationY: 0,
-            rotationZ: 0,
-            opacity: 1,
-            duration: 1.2,
-            ease: "back.out(1.7)",
-            stagger: { each: 0.03, from: "random" },
-          })
+      const runAnimation = contextSafe(() => {
+        const tl = gsap.timeline({ onComplete: () => onComplete?.() });
+
+        tl.to(split.chars, {
+          x: 0,
+          y: 0,
+          z: 0,
+          rotationX: 0,
+          rotationY: 0,
+          rotationZ: 0,
+          opacity: 1,
+          duration: 1.3,
+          ease: "power4.out",
+          stagger: { each: 0.035, from: "random" },
+        })
+          .to(
+            split.chars,
+            {
+              filter: "blur(0px)",
+              duration: 0.9,
+              ease: "power2.out",
+              stagger: { each: 0.035, from: "random" },
+            },
+            "<0.1"
+          )
           .to(containerRef.current, { autoAlpha: 0, duration: 0.6 }, "+=0.3");
       });
+
+      Promise.all([document.fonts.ready]).then(runAnimation);
 
       return () => split.revert();
     },
